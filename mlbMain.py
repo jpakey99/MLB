c32becdda91bed724a6ba4d24b0402f444f66cf5
jpakey99 <jpakery99@gmail.com>
2020-02-08 12:37:19 -0500

    runs a simulation over the course of the 2020 MLB season with even odds for both teams.  Currently outputs the pirates win total

--- mlbMain.py
+++ mlbMain.py
@@ -1,5 +1,25 @@
 import mlbSingleSimulationEO as mlbSSEO
+import statsapi
 
-game = mlbSSEO.SingleGameSimulationEO("Pirates", "Reds")
-game.runSimulation()
-print(game.winningTeam)
+# game = mlbSSEO.SingleGameSimulationEO("Pirates", "Reds")
+# game.runSimulation()
+# print(game.winningTeam)
+
+pirates = 0
+
+def calcWinner(game):
+    global pirates
+    if game.winningTeam == 134:
+        pirates = pirates + 1
+
+
+def simSeason():
+    schedule = statsapi.schedule(start_date='03/26/2020', end_date='09/28/2020')
+
+    for games in schedule:
+        game = mlbSSEO.SingleGameSimulationEO(games['home_id'], games['away_id'])
+        game.runSimulation()
+        calcWinner(game)
+
+simSeason()
+print(pirates)
--- mlbSingleSimulationEO.py
+++ mlbSingleSimulationEO.py
@@ -1,12 +1,21 @@
+"""
+Author: John Akey <jpakey99@gmail.com>
+"""
 from random import seed
 from random import random
 
+"""
+The class will simulate one game with 50/50 odds for each team to win the game.
+homeTeam/awayTeam can either be team ids or team names
+"""
 class SingleGameSimulationEO:
 
+    #initalizes the object
     def __init__(self, homeTeam, awayTeam):
         self.homeTeam = homeTeam
         self.awayTeam = awayTeam
-
+    
+    #runs the simulation.  away team wins if random returns a number less than .5
     def runSimulation(self):
         if random() < .5:
             self.winningTeam = self.awayTeam
