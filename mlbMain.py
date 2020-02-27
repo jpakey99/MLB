34d32ccf385863d3b9f03649312d66c23ac234ab
jpakey99 <jpakery99@gmail.com>
2020-02-27 13:19:38 -0500

    Finished the Playoffs simulation.
    Next step(s): create a main function that will ask for user input.  Simulation can either be 1 season or simming the season multiple times.  In the case of simming the season multiple times, information will need to be stored and than printed at the end of all the simulations.  Would also like to add a home field advantge to the simualtion of the games

--- mlbMain.py
+++ mlbMain.py
@@ -31,13 +31,22 @@ def sim_playoff_series(higher_seed, lower_seed, first_to, total_games, standings
     lower_seed_wins = 0
     games = {'game_date': '00-00-00'}
     for game_num in range(0, total_games):
-        games['home_id'] = higher_seed
-        games['away_id'] = lower_seed
-        game = lSim.LaplaceSim(games, standings)
-        standings = game.run_simulation()
+        #The higher seed is the home team in all the below scenerios
+        if game_num == 0 or game_num == 1 and game_num == 5 and game_num == 6 and (game_num == 4 and total_games == 7):
+            games['home_id'] = higher_seed
+            games['away_id'] = lower_seed
+            game = lSim.LaplaceSim(games, standings)
+            standings = game.run_simulation()
+        #The higher seed is the away team if the above scenerios are not met
+        else:
+            games['home_id'] = lower_seed
+            games['away_id'] = higher_seed
+            game = lSim.LaplaceSim(games, standings)
+            standings = game.run_simulation()
+        #Add the win to the correct team
         if game.losing_team == higher_seed: lower_seed_wins += 1
         else: higher_seed_wins += 1
-
+        #return the result if the a team has won the seres
         if higher_seed_wins == first_to: return standings, lower_seed
         elif lower_seed_wins == first_to: return standings, higher_seed
         else: pass
@@ -54,30 +63,59 @@ def simSeason():
     standings.print_standings()
     #NL wildcard game
     standings, losing_team = sim_playoff_series(standings.nl_seeding[3][0], standings.nl_seeding[4][0], 1,1, standings)
-    if losing_team == standings.nl_seeding[3][0]:
-        del standings.nl_seeding[3]
-    else:
-        del standings.nl_seeding[4]
+    if losing_team == standings.nl_seeding[3][0]: del standings.nl_seeding[3]
+    else: del standings.nl_seeding[4]
+    print("The winner of the NL Wildcard Game: ", standings.teams[standings.nl_seeding[3][0]])
     #AL Wildcard Game
     standings, losing_team = sim_playoff_series(standings.al_seeding[3][0], standings.al_seeding[4][0], 1,1, standings)
-    if losing_team == standings.nl_seeding[3][0]:
-        del standings.al_seeding[3]
-    else:
-        del standings.al_seeding[4]
-    #NL Divisional Round - Series 1
+    if losing_team == standings.nl_seeding[3][0]: del standings.al_seeding[3]
+    else: del standings.al_seeding[4]
+    print("The winner of the AL Wildcard Game: ", standings.teams[standings.al_seeding[3][0]])
+    #NL Divisional Round - Series 1 (1 (0) seed vs 4 (3) seed)
+    standings, losing_team0 = sim_playoff_series(standings.nl_seeding[0][0], standings.nl_seeding[3][0], 3, 5, standings)
+    #NL Divisional Round - Series 2 (2 (1) seed vs 3 (2) seed)
+    standings, losing_team1 = sim_playoff_series(standings.nl_seeding[1][0], standings.nl_seeding[2][0], 3, 5, standings)
+    if losing_team0 == standings.nl_seeding[3][0]: del standings.nl_seeding[3]
+    if losing_team1 == standings.nl_seeding[2][0]: del standings.nl_seeding[2]
+    if losing_team1 == standings.nl_seeding[1][0]: del standings.nl_seeding[1]
+    if losing_team0 == standings.nl_seeding[0][0]: del standings.nl_seeding[0]
+    print("The NL Divisional Winner:", standings.teams[standings.nl_seeding[0][0]])
+    print("The NL Divisional Winner:", standings.teams[standings.nl_seeding[1][0]])
 
-    #NL Divisional Round - Series 2
-
-    #AL Divisional Round - Series 1
-
-    #Al Divisional Round - Series 2
-
+    #AL Divisional Round - Series 1 (1 (0) seed vs 4 (3) seed)
+    standings, losing_team0 = sim_playoff_series(standings.al_seeding[0][0], standings.al_seeding[3][0], 3, 5, standings)
+    #Al Divisional Round - Series 2 (2 (1) seed vs 3 (2) seed)
+    standings, losing_team1 = sim_playoff_series(standings.al_seeding[1][0], standings.al_seeding[2][0], 3, 5, standings)
+    if losing_team0 == standings.al_seeding[3][0]: del standings.al_seeding[3]
+    if losing_team1 == standings.al_seeding[2][0]: del standings.al_seeding[2]
+    if losing_team1 == standings.al_seeding[1][0]: del standings.al_seeding[1]
+    if losing_team0 == standings.al_seeding[0][0]: del standings.al_seeding[0]
+    print("The AL Divisional Winner:", standings.teams[standings.al_seeding[0][0]])
+    print("The AL Divisional Winner:", standings.teams[standings.al_seeding[1][0]])
     #NL Championship Series
-
+    standings, losing_team = sim_playoff_series(standings.nl_seeding[0][0], standings.nl_seeding[1][0], 3, 5, standings)
+    if losing_team == standings.nl_seeding[0][0]:
+        del standings.nl_seeding[0]
+    else:
+        del standings.nl_seeding[1]
+    print("The NL Champion: ", standings.teams[standings.nl_seeding[0][0]])
     #AL Championship Series
-
+    standings, losing_team = sim_playoff_series(standings.al_seeding[0][0], standings.al_seeding[1][0], 3, 5, standings)
+    if losing_team == standings.al_seeding[0][0]:
+        del standings.al_seeding[0]
+    else:
+        del standings.al_seeding[1]
+    print("The AL Champion: ", standings.teams[standings.al_seeding[0][0]])
     #World Series
-
+    wold_series_champion = 0
+    standings, losing_team = sim_playoff_series(standings.al_seeding[0][0], standings.nl_seeding[0][0], 4, 7, standings)
+    if losing_team == standings.al_seeding[0][0]:
+        del standings.al_seeding[0]
+        wold_series_champion = standings.nl_seeding[0][0]
+    else:
+        del standings.nl_seeding[0]
+        wold_series_champion = standings.al_seeding[0][0]
+    print("The World Series Champion: ", standings.teams[wold_series_champion])
 
 
 
