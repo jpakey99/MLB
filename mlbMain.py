31b72b5a4fd5ff388bb59a74bf8a14230405fc69
jpakey99 <jpakery99@gmail.com>
2020-02-27 12:32:48 -0500

    Refactored the code inside SimSeason.  There are now two helper functions: sim_regular_season and sim_playoff_round.  sim_regular_season is complete, at least for the purpose of the current program.  sim_playoff_round will need more work was I implement the further rounds

--- mlbMain.py
+++ mlbMain.py
@@ -8,40 +8,75 @@ def get_schedule(startdate, enddate):
     The 2020 MLB season goes from '03/26/2020' to '09/28/2020'"""
     return statsapi.schedule(start_date=startdate, end_date=enddate)
 
-def simSeason():
-    schedule = get_schedule('03/26/2020', '09/28/2020')
-    standings = Standings.Standings()
-
-    #Regular Season games
+def sim_regular_season(schedule, standings):
+    """Sims a full MLB regular season
+    schedule - the full regular season schedule
+    standings - the standings aboject to be used throughout the season
+    return - standings object"""
     for games in schedule:
         if games['home_id'] != 160:
             game = lSim.LaplaceSim(games, standings)
             standings = game.run_simulation()
+    return standings
+
+def sim_playoff_series(higher_seed, lower_seed, first_to, total_games, standings):
+    """Sims 1 playoff round
+    high_seed - team_id of the higher seeded team (team with the lower index in the seeding list
+    lower_seed - team_id of the lower seeded team (team with the higher index in the seeding list
+    first_to - how many wins a team needs in order to move on
+    total_games - the amount of games played in the series
+    standings - the standings object used throughout the season
+    return - standings and the team_id that losses the series"""
+    higher_seed_wins = 0
+    lower_seed_wins = 0
+    games = {'game_date': '00-00-00'}
+    for game_num in range(0, total_games):
+        games['home_id'] = higher_seed
+        games['away_id'] = lower_seed
+        game = lSim.LaplaceSim(games, standings)
+        standings = game.run_simulation()
+        if game.losing_team == higher_seed: lower_seed_wins += 1
+        else: higher_seed_wins += 1
+
+        if higher_seed_wins == first_to: return standings, lower_seed
+        elif lower_seed_wins == first_to: return standings, higher_seed
+        else: pass
+
+def simSeason():
+    schedule = get_schedule('03/26/2020', '09/28/2020')
+    standings = Standings.Standings()
+
+    standings = sim_regular_season(schedule, standings)
 
     #End of regular season, get the final results for the playoffs
     standings.create_divisions()
     standings.playoff_seeding()
     standings.print_standings()
-    #Begin the Playoffs with wildcard team
-    games = {'game_date': '00-00-00'}
     #NL wildcard game
-    games['home_id'] = standings.nl_seeding[3][0]
-    games['away_id'] = standings.nl_seeding[4][0]
-    game = lSim.LaplaceSim(games, standings)
-    standings = game.run_simulation()
-    if game.losing_team == standings.nl_seeding[3][0]:
+    standings, losing_team = sim_playoff_series(standings.nl_seeding[3][0], standings.nl_seeding[4][0], 1,1, standings)
+    if losing_team == standings.nl_seeding[3][0]:
         del standings.nl_seeding[3]
     else:
         del standings.nl_seeding[4]
     #AL Wildcard Game
-    games['home_id'] = standings.al_seeding[3][0]
-    games['away_id'] = standings.al_seeding[4][0]
-    game = lSim.LaplaceSim(games, standings)
-    standings = game.run_simulation()
-    if game.losing_team == standings.nl_seeding[3][0]:
-        del standings.nl_seeding[3]
+    standings, losing_team = sim_playoff_series(standings.al_seeding[3][0], standings.al_seeding[4][0], 1,1, standings)
+    if losing_team == standings.nl_seeding[3][0]:
+        del standings.al_seeding[3]
     else:
-        del standings.nl_seeding[4]
+        del standings.al_seeding[4]
+    #NL Divisional Round - Series 1
+
+    #NL Divisional Round - Series 2
+
+    #AL Divisional Round - Series 1
+
+    #Al Divisional Round - Series 2
+
+    #NL Championship Series
+
+    #AL Championship Series
+
+    #World Series
 
 
 
