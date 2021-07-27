e4a247ae56c19b4fd19e443f46a006f27ffc6322
jpakey99 <jpakey99@gmail.com>
2021-07-26 21:11:39 -0400

    Grade system created and implemented,  Need to create report system
    Signed-off-by: jpakey99 <jpakey99@gmail.com>

--- prospects/prospects.py
+++ prospects/prospects.py
@@ -1,8 +1,9 @@
 import numpy as np
 from statistics import stdev
-import matplotlib
+import matplotlib, statistics
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
+import scipy.stats as stats
 # read from a file
 # get mean, std deviation for entire league
 # calculate z-scores for entire league
@@ -54,14 +55,33 @@ def plot_histogram(data):
     fig.show()
 
 
+def normaldist(mean, std, value):
+    return stats.norm.cdf((value-mean)/std)
+
+
 if __name__ == '__main__':
     prospects = read_file('AAA_East.csv')
-    data = []
+    data, age = [], []
     for prospect in prospects:
         data.append(float(prospect[12]))
+        age.append(int(prospect[4]))
     mean, std = get_statistics(data)
-    z_scores = []
+    age_mean, age_std = get_statistics(age)
+    z_scores, age_z_scores = [], []
     for datum in data:
         z_scores.append(find_z_score(mean, datum))
+    for datum in age:
+        age_z_scores.append(find_z_score(age_mean, datum))
+    z_mean, z_std = get_statistics(z_scores)
+    age_z_mean, age_z_std = get_statistics(age_z_scores)
     print(z_scores)
-    fig = plot_histogram(z_scores)
+    # fig = plot_histogram(z_scores)
+    normal_values = []
+    for z in z_scores:
+        normal_values.append(normaldist(z_mean, z_std, z))
+    n_mean, n_std = get_statistics(normal_values)
+    for i, n in enumerate(normal_values):
+        x = (n - n_mean) / n_std
+        value = (round(x, 1)*3)
+        scout = 50 + (5 * value)
+        print(scout, prospects[i][1])
